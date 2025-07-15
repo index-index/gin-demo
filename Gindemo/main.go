704932ac6fbfec0daafced527ac6b2fddc1fe102
wowowo11 <3398528263@qq.com>
2025-07-15 16:41:42 +0800

    three save

--- Gindemo/main.go
+++ Gindemo/main.go
@@ -1,15 +1,58 @@
 package main
+
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
+	"github.com/thinkerou/favicon"
+	"encoding/json"
 )
+
 func main() {
-	//创建默认的Gin引擎
-	r := gin.Default()
-	//定义一个GET请求的路由
-	r.GET("/hello", func(c *gin.Context)){
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello Gin!",
+	//创建一个服务
+	ginServer := gin.Default()
+	ginServer.Use(favicon.New("favicon.ico"))
+	//加载静态页面
+	ginServer.LoadHTMLGlob("templates/*")
+	//加载资源文件
+	ginServer.Static("/static", "./static")
+	//相应一个页面给前端
+	ginServer.GET("/index", func(context *gin.Context){
+		//context.JSON() json数据
+		context.HTML(200, "index.html", gin.H{
+			"msg" : "这是一个go后台传递来的数据",
+		})
+		})
+		//usl? userid=xxx&username=index
+		ginServer.GET("/user/info", func(context *gin.Context) {
+		userid := context.Query("userid")
+		username := context.Query("username")
+		context.JSON(200, gin.H{
+			"userid":  userid,
+			"username": username,
+		})
 	})
-	r.Run(":8080")
+		// /user/info/1/index
+		ginServer.GET("/user/info/:userid/:username", func(context *gin.Context){
+		userid := context.Param("userid")
+		username := context.Param("username")
+			context.JSON(200, gin.H{
+				"userid" : userid,
+				"username" : username,
+		})
+	})
+
+	//前端给后端传递json
+	ginServer.POST("/json", func(context *gin.Context) {
+    // request body
+    data, _ := context.GetRawData()
+
+    var m map[string]interface{}
+    if err := json.Unmarshal(data, &m); err != nil {
+        context.JSON(200, gin.H{"error": "Invalid JSON"})
+        return
+    }
+    context.JSON(200, m) // 确保 m 是一个 object 类型的数据
+})
+	//服务器端口
+	ginServer.Run(":8082")
+
 }
